34a7231f935822527f05e0cbca74d0f9a30f0ec3
Khanh <Khanh13Nguyen13@gmail.com>
2020-08-22 00:58:52 +0300

    HW-1: Get fname/lname/age user

---
+++ home-work-1/index.js
@@ -0,0 +1,5 @@
+let firstName = prompt('Ваше имя:', '');
+let lastName = prompt('Ваше фамилия:', '');
+let age = Number(prompt('Ваш возраст:', ''));
+
+alert(`Пользователь: ${firstName} ${lastName} \n Возраст: ${age}`)
