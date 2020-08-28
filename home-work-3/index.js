265d8e5fe50abc6159a8a332b97065e6bdc05a07
Khanh <Khanh13Nguyen13@gmail.com>
2020-08-29 00:01:29 +0300

    Try again

--- home-work-3/index.js
+++ home-work-3/index.js
@@ -1,35 +1,40 @@
-// let number = +prompt('Введите число болье 10:', '');
-// let sum = 0;
+// ======= *Основное задание* =======
 
-// if (number > 10) {
+function sum() {
+  let number = +prompt('Введите число больше 10:', '');
+  let sum = 0;
+  
+  if (number > 10) {
+  
+    for (let i = 1; i < number + 1; i++) {
+      sum += i;
+      console.log(i);
+    }
+  
+    console.log(`Сумма чисел: ${sum}`);
+  }
+}
+// sum();
 
-//   for (let i = 1; i < number + 1; i++) {
-//     sum += i;
-//     console.log(i);
-//   }
-
-//   console.log(`Сумма чисел: ${sum}`);
-// }
 
 
-// ========== *Дополнительное задание* ==========
+// ======= *Дополнительное задание* =======
 
 const COLOR_PINK = 'pink';
 
 function guessColor() {
   let counter = 0;
-  
-  while (counter < 3) {
-    let colors = prompt('Угадай один из цветов: red, black, blue, pink, green', '');
+  let colors;
 
+  while (counter < 3) {
+    colors = prompt('Угадай один из цветов: red, black, blue, pink, green', '');
+    
     if (colors == COLOR_PINK) {
       alert(`Вы угадали: ${COLOR_PINK}`)
       break;
-    } else {
-      console.log(counter);
     }
 
     counter++;
   }
 }
-guessColor();
+// guessColor();
