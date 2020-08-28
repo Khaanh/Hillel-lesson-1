02cdbae9a0e853838b8cd1bc0365b4729726de13
Khanh <Khanh13Nguyen13@gmail.com>
2020-08-29 00:53:59 +0300

    HW: Calculator

--- home-work-2/index.js
+++ home-work-2/index.js
@@ -1,82 +1,90 @@
-// let op1 = +prompt('Введите первое число:', ''),
-//     op2 = +prompt('Введите второе число:', ''),
-//     sign = prompt('знак между ними:', '');
+// ======= *Основное задание* =======
 
-// switch (sign) {
-//   case '-': {
-//     console.log(op1 - op2);
-//     break;
-//   }
+function calc() {
 
-//   case '+': {
-//     console.log(op1 + op2);
-//     break;
-//   }
+  let op1 = +prompt('Введите первое число:', ''),
+      op2 = +prompt('Введите второе число:', ''),
+      sign = prompt('знак между ними:', '');
+  
+  switch (sign) {
+    case '-': {
+      alert(op1 - op2);
+      break;
+    }
+  
+    case '+': {
+      alert(op1 + op2);
+      break;
+    }
+  
+    case '/': {
+      alert(op1 / op2);
+      break;
+    }
+  
+    case '*': {
+      alert(op1 * op2);
+      break;
+    }
+  
+    case '**': {
+      alert(op1 ** op2);
+      break;
+    }
+  
+    case '%': {
+      alert(op1 % op2);
+      break;
+    }
+  
+    default: {
+      alert('Что-то пошло не так...');
+    }
+  }
+}
+// calc();
 
-//   case '/': {
-//     console.log(op1 / op2);
-//     break;
-//   }
-
-//   case '*': {
-//     console.log(op1 * op2);
-//     break;
-//   }
-
-//   case '**': {
-//     console.log(op1 ** op2);
-//     break;
-//   }
-
-//   case '%': {
-//     console.log(op1 % op2);
-//     break;
-//   }
-
-//   default: {
-//     console.log('Что-то пошло не так...');
-//   }
-// }
-
-// ========== *Дополнительное задание* ==========
+// ======= *Дополнительное задание* =======
 
 
 let sub = function(n1, n2) {
-  console.log(n1 - n2);
+  alert(n1 - n2);
 }
 
 let sum = function(n1, n2) {
-  console.log(n1 + n2);
+  alert(n1 + n2);
 }
 
 let mul = function(n1, n2) {
-  console.log(n1 * n2);
+  alert(n1 * n2);
 }
 
 let div = function(n1, n2) {
-  console.log(n1 / n2);
+  alert(n1 / n2);
 }
 
 function calculate(n1, n2, sign) {
   n1 = +prompt('Введите число n1:', ''),
   n2 = +prompt('Введите число n2:', ''),
-  sign = prompt('Введите арифметический знак:', '');
+  sign;
   
-  console.log(n1);
-  console.log(n2);
-  console.log(sign);
-  if (sign == '-') {
-    sub(n1, n2);
-  } else if (sign == '+') {
-    sum(n1, n2);
-  } else if (sign == '*') {
-    mul(n1, n2);
-  } else if (sign == '/') {
-    div(n1, n2)
-  } else if (sign == '') {
-    console.log(' Вы забыли ввести знак ');
-  } else if ( !((typeof n1) == 'number') ) {
-    console.log('n1 - Не число');
+  if ( (isNaN(n1)) || (isNaN(n2)) ) {
+    alert('n1 или n2 не является числом!')
+  } else {
+    sign = prompt('Введите арифметический знак:', '');
+    if ( sign == '-') {
+      sub(n1, n2)
+    } else if (sign == '+') {
+        sum(n1, n2)
+      } else if (sign == '*') {
+        mul(n1, n2)
+      } else if (sign == '/') {
+        div(n1, n2)
+      } else if (sign == '') {
+        alert('Вы не ввели знак')
+      } else {
+        alert('Вы ввели что-то непонятное...')
+      }
   }
 }
-// calculate();
+calculate();
