2b26233fd984337616da04505068a46805af912f
Khanh <Khanh13Nguyen13@gmail.com>
2020-09-05 11:47:29 +0300

    Step by step

--- home-work-7/index.js
+++ home-work-7/index.js
@@ -5,49 +5,42 @@
  * @returns {array} - новый массив
  */
 
-// "откидывать" все значение, который в логическом контексте false (т.е все негативные)
-// если элемент массива число или true - заменить на объект
-
-// Функция explainer должна
-
-// "откидывать" все значение, который в логическом контексте false (т.е все негативные)
-// если элемент массива число или true - заменить на объект
-//     { type: 'number/boolean', value: число из массива / true}
-// если элемент строка - заменить на объект
-//     { type: 'string', value: строка из массива, length: длина строки}
-
  let arr = [10, 'Hello world', null, true , '', false, 255];
  let obj = {};
+ let objStr = {};
+ let objNumber = {}
 
- function explainer(arr, obj) {
-
-  let result = arr.filter(function (item, index) {
+ function explainer(arr, objNumber, objStr) {
+  console.log(arr);
+  // Получаем в result значения, которые в лог. контексте true
+  let result = arr.filter(function (item) {
     return Boolean (item)
   })
+  let newArr = [];
 
+  
+  // перебираем все "положительные" значения из переменной result
   for (let i = 0; i < result.length; i++) {
-    // console.log(result[i]);
 
-    if ( isNaN(result[i]) ) {
-      obj.type = 'string';
-      obj.value = result[i];
-      obj.length = result[i].length;
-      console.log(`result[i]: ${result[i]}`);
-      console.log(obj);
+    if (typeof (result[i]) == 'string') {
+      console.log(result[i]);
+      objStr.type = 'string',
+      objStr.value = result[i],
+      objStr.length = result[i].length,
+      console.log(objStr);
+      newArr.push(objStr)
+    } else if ((typeof (result[i]) == 'number') || typeof (result[i]) == 'boolean') {
+      console.log(result[i]);
+      objNumber.type = 'number/boolean',
+      objNumber.value = result[i],
+      console.log(objNumber);
+      newArr.push(objNumber)
+    } else {
+      return;
     }
   }
 
-  // console.log(result);
-  // console.log(obj);
-  
+  return newArr
  }
 
- explainer(arr, obj);
-
-
- console.log('----------------------');
-
- console.log(isNaN(10)); //false
- console.log(isNaN('Hello world')); //true
- console.log(isNaN(true)); //false
- console.log(isNaN(255)); //false
+explainer(arr, objNumber, objStr);
