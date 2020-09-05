e207b93302e01f4bf169c1bbe7997224e6cdf0db
Khanh <Khanh13Nguyen13@gmail.com>
2020-09-05 16:35:45 +0300

    function explainer - done

--- home-work-7/index.js
+++ home-work-7/index.js
@@ -1,46 +1,38 @@
 /**
- * Функция explainer сделает НОВЫЙ "обяъснительный" массив
- * 
- * @param {array} - массив
- * @returns {array} - новый массив
- */
+* Функция explainer сделает НОВЫЙ "обяъснительный" массив
+* 
+* @param {array} - массив
+* @returns {array} - новый массив
+*/
 
- let arr = [10, 'Hello world', null, true , '', false, 255];
- let obj = {};
- let objStr = {};
- let objNumber = {}
+let arr = [10, 'Hello world', null, true , '', false, 255];
 
- function explainer(arr, objNumber, objStr) {
-  console.log(arr);
-  // Получаем в result значения, которые в лог. контексте true
-  let result = arr.filter(function (item) {
-    return Boolean (item)
-  })
+function explainer(arr) {
   let newArr = [];
 
+  // Получаем в result значения, которые в лог. контексте true
+  let result = arr.filter(function (item) {
+    return Boolean (item);
+  })
   
   // перебираем все "положительные" значения из переменной result
   for (let i = 0; i < result.length; i++) {
+    let objNumber = {};
+    let objStr = {};
 
     if (typeof (result[i]) == 'string') {
-      console.log(result[i]);
-      objStr.type = 'string',
+      objStr.type = 'String',
       objStr.value = result[i],
       objStr.length = result[i].length,
-      console.log(objStr);
       newArr.push(objStr)
-    } else if ((typeof (result[i]) == 'number') || typeof (result[i]) == 'boolean') {
-      console.log(result[i]);
-      objNumber.type = 'number/boolean',
-      objNumber.value = result[i],
-      console.log(objNumber);
+    } else if (typeof (result[i]) == 'number' || typeof (result[i]) == 'boolean') {
+      objNumber.type = 'Number/Boolean',
+      objNumber.value = result[i]
       newArr.push(objNumber)
-    } else {
-      return;
     }
   }
-
+  
   return newArr
- }
+}
 
-explainer(arr, objNumber, objStr);
+explainer(arr);
