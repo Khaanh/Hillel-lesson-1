431af8b6c0d3cde46779c2db7fb775124fd29267
Khanh <Khanh13Nguyen13@gmail.com>
2020-09-08 23:59:25 +0300

    function customMap()

--- home-work-8/index.js
+++ home-work-8/index.js
@@ -1,24 +1,19 @@
 let frontend = ['I am', 'learing', 'JavaScript', 'by', 'practice', 'every', 'day'];
 
 
-// Напишите функцию getYoungEmployees(), которая отфильтрует работников возрастом меньше 30 и вернет новый строковой массив ТОЛЬКО их имен.
+function customMap(array, cb) {
+  
+  let item = []
 
-// ==================================================================
-let vasia = { name: "Вася", age: 25 };
-let petia = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-let egor = { name: "Егор", age: 40 };
-let dima = { name: "Дима", age: 35 };
-let lena = { name: "Лена", age: 18 };
+  for (let index = 0; index < array.length; index++) {
+    item.push(array[index]);
+    
+    cb(item, index);
+  }
 
-let users = [ vasia, petia, masha, egor, dima, lena ];
-
-function getYoungEmployees(arr) {
-
-  let usersAge = arr.filter(user => user.age < 30);
-  let usersName = usersAge.map(user => user.name)
-
-  return usersName;
+  return item;
 }
 
-getYoungEmployees(users)
+customMap(frontend, function(item, index) {
+  console.log(item[index].length);
+})
