1f2610f1dca7a3cc2e40b294c9546fe12a4c3cb4
Khanh <Khanh13Nguyen13@gmail.com>
2020-10-06 00:33:45 +0300

    Step by step

--- home-work-16/index.js
+++ home-work-16/index.js
@@ -1,133 +1,142 @@
+let formInfo = document.querySelector('#form-info');
+let saveInfo = document.querySelector('#save-info');
+let btnUpdate = document.querySelector('#update-btn');
+let listGroup = document.querySelector('#list-group');
 
-let getInfo = document.getElementById('get-info');
-let group = [];
+let name = formInfo.elements.name;
+let surname = formInfo.elements.surname;
+let age = formInfo.elements.age;
+let scores = formInfo.elements.scores;
+let groupList = [];
+let student;
+let teacher;
+let updateList = [];
 
-getInfo.onclick = function() {
-  let formInfo = document.getElementById('form-info');
-  let formInfoChildren = formInfo.children;
-  console.log(formInfoChildren.fName.value);
-  console.log(formInfoChildren.lName.value);
-  console.log(formInfoChildren.age.value);
-  console.log(formInfoChildren.scores.value);
+// let li = document.createElement('li');
 
-  let student = new Student({
-    fname: formInfoChildren.fName.value,
-    lname: formInfoChildren.lName.value,
-    age: formInfoChildren.age.value,
-    scores: formInfoChildren.scores.value,
-  })
+saveInfo.onclick = function() {
+  student = new Student ({
+    name: name.value,
+    surname: surname.value,
+    age: age.value,
+    scores: scores.value.split(', ').map(item => Number(item)),
+  });
+  groupList.push(student);
+  
+  teacher = new Teacher ({
+    name: 'Teacher',
+    surname: 'Surname Teacher',
+    age: 56,
+    group: groupList, 
+  });
 
-  group.push(student)
-  console.log(student, group);
-  let teacher = new Teacher(group);
-  console.log(teacher);
+  // console.log(teacher.getListOfNamesByAverageMark());
 }
 
-class Student {
+
+let resultList = [];
+
+btnUpdate.onclick = function() {
+  console.log(teacher.getListOfNamesByAverageMark());
+  // teacher.getListOfNamesByAverageMark()
+  resultList.forEach(student => {
+    let li = document.createElement('li');
+    li.append(`Имя: ${student}`)
+    // li.append(`Имя: ${student.name} Фамилия: ${student.surname} Средняя оценка:${student.averageMark()}`);
+    // li.append(teacher.getListOfNamesByAverageMark());
+    listGroup.append(li)
+    // console.log(teacher.getListOfNamesByAverageMark());
+  })
+  groupList = [];
+  console.log(resultList);
+}
+
+
+// listGroup.children[i].innerHTML = `<strong> Имя студента: ${teacher.group[i].name}  ${teacher.group[i].surname}</strong> <br> Оценка студента:
+// ${student.averageMark()}`
+
+
+
+
+// ==========================
+class Human {
   constructor(config) {
-    this.fname = config.fname
-    this.lname = config.lname
-    this.age = config.age
-    this.scores = config.scores
+    this.name = config.name;
+    this.surname = config.surname;
+    this.age = config.age;
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+
+  setFullName(fullName) {
+    fullName = fullName.split(" ");
+    this.name = fullName[0];
+    this.surname = fullName[1];
   }
 }
 
-class Teacher {
-  constructor(group) {
-    this.group = group;
+class Student extends Human{
+  constructor({name, surname, age, scores}) {
+    super({name, surname, age})
+    this.scores = scores;
   }
 
   averageMark() {
-    return this.scores.reduce((acc, curentVal) => acc + curentVal) / this.scores.length;
+    return Math.ceil(this.scores.reduce((acc, curentVal) => acc + curentVal) / this.scores.length);
   }
 
+  minMark() {
+    return this.scores.sort((a, b) => b - a)[this.scores.length - 1];
+  }
 
+  maxMark() {
+    return this.scores.sort((a, b) => a - b)[this.scores.length - 1];
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname} - student`;
+  }
+}
+
+class Teacher extends Human{
+  constructor({name, surname, age, group}) {
+    super({name, surname, age})
+    this.group = group;
+  }
+
+  getListOfNamesByAverageMark() {
+    return this.group
+      .sort((a, b) => b.averageMark() - a.averageMark())
+      .map((student) => `${student.name} - ${student.surname}`);
+  }
+
+  getStudentByName(name) {
+    return this.group.filter((item) => item.name == name)
+  }
+
+  removeStudentByName(name) {
+    let index = this.group.filter((item) => item.name == name);
+    return this.group.splice(this.group.indexOf(index), 1);
+  }
+  
+  updateStudentByName(student, name) {
+    this.group.splice(
+      this.group.indexOf(this.getStudentByName(name)),
+      1,
+      new Student(student)
+    );
+  }
 }
 
 
-let teacher2 = new Teacher(222);
-
-
-
-// class Human {
-//   constructor(config) {
-//     this.name = config.name;
-//     this.surname = config.surname;
-//     this.age = config.age;
-//   }
-
-//   getFullName() {
-//     return `${this.name} ${this.surname}`;
-//   }
-
-//   setFullName(fullName) {
-//     fullName = fullName.split(" ");
-//     this.name = fullName[0];
-//     this.surname = fullName[1];
-//   }
-
-// }
-
-// class Student extends Human{
-//   constructor({name, surname, age, scores}) {
-//     super({name, surname, age})
-//     this.scores = scores;
-//   }
-
-//   averageMark() {
-//     return this.scores.reduce((acc, curentVal) => acc + curentVal) / this.scores.length;
-//   }
-
-//   minMark() {
-//     return this.scores.sort((a, b) => b - a)[this.scores.length - 1];
-//   }
-
-//   maxMark() {
-//     return this.scores.sort((a, b) => a - b)[this.scores.length - 1];
-//   }
-
-//   getFullName() {
-//     return `${this.name} ${this.surname} - student`;
-//   }
-// }
-
-// class Teacher extends Human{
-//   constructor({name, surname, age, group}) {
-//     super({name, surname, age})
-//     this.group = group;
-//   }
-
-//   getListOfNamesByAverageMark() {
-//     return this.group
-//       .sort((a, b) => b.averageMark() - a.averageMark())
-//       .map((student) => student.name);
-//   }
-
-//   getStudentByName(name) {
-//     return this.group.filter((item) => item.name == name)
-//   }
-
-//   removeStudentByName(name) {
-//     let index = this.group.filter((item) => item.name == name);
-//     return this.group.splice(this.group.indexOf(index), 1);
-//   }
-  
-//   updateStudentByName(student, name) {
-//     this.group.splice(
-//       this.group.indexOf(this.getStudentByName(name)),
-//       1,
-//       new Student(student)
-//     );
-//   }
-// }
-
-// let student = new Student({
-//   name: 'Student',
-//   surname: 'Student surname',
-//   age: 26,
-//   scores: [1,2,3,4,5],
+// let teacher = new Teacher({
+//   name: 'Teacher',
+//   surname: 'Teacher Surname',
+//   age: 46,
+//   group: groupList,
 // })
-
 
 // let human = new Human({
 //   name: 'Khanh',
