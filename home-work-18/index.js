779439298653e532de6b50fcf0685cd9a23c2cb0
Khanh <Khanh13Nguyen13@gmail.com>
2020-10-07 01:38:58 +0300

    Order pizza

--- home-work-18/index.js
+++ home-work-18/index.js
@@ -4,23 +4,88 @@ let ingredientsList = document.querySelectorAll('.ingredients-input');
 let orderBtn = document.querySelector('#btn-order');
 
 
-let ingredientsArr = [];
-// По клику получаем значение из radio и сохраняем в переменную
+// По клику получаем значение из radio/checkbox и сохраняем в переменную
 orderBtn.onclick = function(e) {
-  let pizzaSizeValue = 0;
-  // for (let i = 0; i < pizzaSize.length; i++) {
-  //   if (pizzaSize[i].checked == true) {
-  //     pizzaSizeValue = pizzaSize[i].value;
-  //     console.log(pizzaSizeValue);
-  //   }
-  // }
-
+  e.preventDefault();
+  
+  let pizzaSizeValue = 0
+      ingredientsArr = [];
+  
+  // Выбор размера пиццы
+  for (let i = 0; i < pizzaSize.length; i++) {
+    if (pizzaSize[i].checked == true) {
+      pizzaSizeValue = pizzaSize[i].value;
+      console.log(pizzaSizeValue);
+    }
+  }
+  
+  // Выбор ингредиентов
   for (let i = 0; i < ingredientsList.length; i++) {
     if (ingredientsList[i].checked == true) {
+      ingredientsArr.push(ingredientsList[i]);
     }
-    ingredientsArr.push(ingredientsList[i]);
-    console.log(ingredientsList[i]);
-    console.log(ingredientsArr);
   }
-  e.preventDefault();
+  
+  checkIngredients(ingredientsArr, pizzaSizeValue)
 }
+
+/**
+* checkIngredients - проверяем кол-во ингредиентов
+* @param {array} arr - массив ингредиентов
+* @param {string} selected - размер выбранной пиццы
+*/
+
+function checkIngredients(arr, selected) {
+  let infoMessage = document.querySelector('.info-block');
+  
+  if (arr.length >= 3) {
+    infoMessage.classList.remove('is-warning')
+    return confirmOrder(true, arr, selected)
+  } else {
+    infoMessage.classList.add('is-warning')
+  }
+}
+
+/**
+* confirmOrder - выводим сообщение с информацией о заказе и подтверждение
+* @param {boolean} status - статус о прохождения валидации на 3 ингрединета
+* @param {array} ingredients - массив ингредиентов
+* @param {string} size - размер выбранной пиццы
+*/
+
+function confirmOrder(status, ingredients, size) {
+  let arrDescr = [],
+      strArrDescr = '',
+      agreement = false;
+  
+  for (let i = 0; i < ingredients.length; i++) {
+    arrDescr.push(ingredients[i].value);
+  }
+  strArrDescr = arrDescr.join(', ');
+  
+  if (status) {
+    return agreement = confirm(`Your order: ${size} pizza with ${strArrDescr}, all right?`)
+  }
+
+  return agreement;
+}
+
+
+// let test = document.querySelector('#test');
+
+// test.onclick = function() {
+//   let a = first();
+
+//   second(a)
+// }
+
+// function first() {
+//   alert('first');
+//   return false;
+// }
+
+// function second(stat) {
+//   if (stat) {
+//     alert('second')
+//   }
+// }
