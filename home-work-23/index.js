719faef6d94a6ccb0f050379fde72a76b59970ad
Khanh <Khanh13Nguyen13@gmail.com>
2020-11-07 22:31:21 +0200

    Validate form

--- home-work-23/index.js
+++ home-work-23/index.js
@@ -1,28 +1,59 @@
 let form      = document.querySelector('#form');
 let btnSubmit = document.querySelector('#btn-submit');
-let inputForm = form.elements;
-let inputEmail = inputForm.email;
-let inputName = inputForm.name;
-let inputPass = inputForm.pass;
-let inputRepeat = inputForm.repeat;
+let inputEmail = form.elements.email;
+let inputName = form.elements.name;
+let inputPass = form.elements.pass;
+let inputRepeat = form.elements.repeat;
 let objResult = {};
-
+let span = document.createElement('span');
+let emailError = 'Неправильный формат почты';
+let nameError = 'Вы пропустили имя';
+let passError = 'Пароль должен быть длиней 6 символов';
+let repeatPassError = 'Пароли не совпадают';
+let password;
+    
 form.oninput = function(e) {
+    
     if (inputEmail.value.includes('@')) {
-        e.target.classList.add('_is-success')
+        inputEmail.classList.add('_is-success')
     } else {
-        e.target.classList.remove('_is-success')
+        inputEmail.classList.remove('_is-success')
     }
 
-    if (inputName.value.length != 0) {
-        e.target.classList.add('_is-success')
+    if (inputName.value.length) {
+        inputName.classList.add('_is-success')
     } else {
-        e.target.classList.remove('_is-success')
+        inputName.classList.remove('_is-success')
     }
 
     if (inputPass.value.length > 3) {
-        e.target.classList.add('_is-success')
+        password = inputPass.value;
+        inputPass.classList.add('_is-success')
     } else {
-        e.target.classList.remove('_is-success')
+        inputPass.classList.remove('_is-success')
     }
+
+    if (inputRepeat.value == password) {
+        inputRepeat.classList.add('_is-success')
+    } else {
+        inputRepeat.classList.remove('_is-success')
+    }
+
+    if ((inputEmail.classList.contains('_is-success')) &&
+        (inputName.classList.contains('_is-success')) &&
+        (inputPass.classList.contains('_is-success')) &&
+        (inputRepeat.classList.contains('_is-success'))) {
+            btnSubmit.removeAttribute('disabled')
+            objResult.email = inputEmail.value;
+            objResult.name = inputName.value;
+            objResult.pass = inputPass.value; 
+            objResult.passRepeat = inputRepeat.value; 
+        } else {
+            btnSubmit.setAttribute('disabled', true)
+        }
 }
+
+form.addEventListener('submit', function(e) {
+    console.dir(objResult);
+    e.preventDefault();
+})
