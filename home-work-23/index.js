6c5d0c980d2c3f3aece5336742087d7126ddb67f
Khanh <Khanh13Nguyen13@gmail.com>
2020-12-02 00:54:07 +0200

    HW:23 fix validate

--- home-work-23/index.js
+++ home-work-23/index.js
@@ -19,85 +19,121 @@ let flagPass = false;
 let flagRepeat = false;
 
 
-function validateForm() {
-    let passValue;
+form.oninput = function(e) {
+    let target = e.target;
 
-    inputEmail.oninput = function(e) {
-        let target = e.target;
-        if (target.value.includes('@')) {
-            target.classList.add('_is-success');
-            flagEmail = true;
-            span.remove();
-        } else {
-            span.classList.add('error');
-            span.innerHTML = emailError;
-            target.after(span);
-            target.classList.add('_is-error')
-            target.classList.remove('_is-success')
-        }
-    }
-    
-    inputName.oninput = function(e) {
-        let target = e.target;
-        if (inputName.value.length > 2) {
-            target.classList.add('_is-success');
-            flagName = true;
-            span.remove();
-        } else {
-            span.classList.add('error');
-            span.innerHTML = nameError;
-            target.after(span);
-            target.classList.add('_is-error')
-            target.classList.remove('_is-success')
-        }
-    }
-    
-    inputPass.oninput = function(e) {
-        let target = e.target;
-        if (inputPass.value.length > 2) {
-            passValue = inputPass.value;
-            target.classList.add('_is-success');
-            flagPass = true;
-            span.remove();
-        } else {
-            span.classList.add('error');
-            span.innerHTML = passError;
-            target.after(span);
-            target.classList.add('_is-error')
-            target.classList.remove('_is-success')
-        }
-    }
-    
-    inputRepeat.oninput = function(e) {
-        let target = e.target;
-        if (inputRepeat.value == passValue) {
-            target.classList.add('_is-success');
-            flagRepeat = true;
-            span.remove();
-        } else {
-            span.classList.add('error');
-            span.innerHTML = repeatPassError;
-            target.after(span);
-            target.classList.add('_is-error')
-            target.classList.remove('_is-success')
-        }
-    }
-}validateForm();
+    console.log(email);
 
-
-form.oninput = function() {
-    if (flagEmail && flagName && flagPass && flagRepeat) {
-        btnSubmit.removeAttribute('disabled')
-        objResult.email = inputEmail.value;
-        objResult.name = inputName.value;
-        objResult.pass = inputPass.value; 
-        objResult.passRepeat = inputRepeat.value; 
+    if (target.type === 'email' && target.value.includes('@')) {
+        target.classList.add('_is-success');
+        console.log(target);
+        flagEmail = true;
+        span.remove();
     } else {
-        btnSubmit.setAttribute('disabled', true)
+        console.log(target);
+        span.classList.add('error');
+        span.innerHTML = emailError;
+        target.after(span);
+        target.classList.add('_is-error')
+        target.classList.remove('_is-success')
     }
+    
+    // if (target.type == 'text' && target.value.length > 2) {
+    //     target.classList.add('_is-success');
+    //     console.log(target);
+    //     flagName = true;
+    //     span.remove();
+    // } else {
+    //     console.log(target);
+    //     span.classList.add('error');
+    //     span.innerHTML = nameError;
+    //     target.after(span);
+    //     target.classList.add('_is-error')
+    //     target.classList.remove('_is-success')
+    // }
+
 }
 
-form.addEventListener('submit', function(e) {
-    console.dir(objResult);
-    e.preventDefault();
-})
+
+// function validateForm() {
+//     let passValue;
+
+//     inputEmail.oninput = function(e) {
+//         let target = e.target;
+//         if (target.value.includes('@')) {
+//             target.classList.add('_is-success');
+//             flagEmail = true;
+//             span.remove();
+//         } else {
+//             span.classList.add('error');
+//             span.innerHTML = emailError;
+//             target.after(span);
+//             target.classList.add('_is-error')
+//             target.classList.remove('_is-success')
+//         }
+//     }
+    
+//     inputName.oninput = function(e) {
+//         let target = e.target;
+//         if (inputName.value.length > 2) {
+//             target.classList.add('_is-success');
+//             flagName = true;
+//             span.remove();
+//         } else {
+//             span.classList.add('error');
+//             span.innerHTML = nameError;
+//             target.after(span);
+//             target.classList.add('_is-error')
+//             target.classList.remove('_is-success')
+//         }
+//     }
+    
+//     inputPass.oninput = function(e) {
+//         let target = e.target;
+//         if (inputPass.value.length > 2) {
+//             passValue = inputPass.value;
+//             target.classList.add('_is-success');
+//             flagPass = true;
+//             span.remove();
+//         } else {
+//             span.classList.add('error');
+//             span.innerHTML = passError;
+//             target.after(span);
+//             target.classList.add('_is-error')
+//             target.classList.remove('_is-success')
+//         }
+//     }
+    
+//     inputRepeat.oninput = function(e) {
+//         let target = e.target;
+//         if (inputRepeat.value == passValue) {
+//             target.classList.add('_is-success');
+//             flagRepeat = true;
+//             span.remove();
+//         } else {
+//             span.classList.add('error');
+//             span.innerHTML = repeatPassError;
+//             target.after(span);
+//             target.classList.add('_is-error')
+//             target.classList.remove('_is-success')
+//         }
+//     }
+// }validateForm();
+
+
+// form.oninput = function() {
+//     if (flagEmail && flagName && flagPass && flagRepeat) {
+//         btnSubmit.removeAttribute('disabled')
+//         objResult.email = inputEmail.value;
+//         objResult.name = inputName.value;
+//         objResult.pass = inputPass.value; 
+//         objResult.passRepeat = inputRepeat.value; 
+//     } else {
+//         btnSubmit.setAttribute('disabled', true)
+//     }
+// }
+
+// form.addEventListener('submit', function(e) {
+//     console.dir(objResult);
+//     e.preventDefault();
+// })
