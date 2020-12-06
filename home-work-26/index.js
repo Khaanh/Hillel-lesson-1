aabd05946ad3dc096b83c90feeebebad125ee857
Khanh <Khanh13Nguyen13@gmail.com>
2020-12-06 02:21:19 +0200

    HW 26: my first request

--- home-work-26/index.js
+++ home-work-26/index.js
@@ -1,14 +1,33 @@
 let requestBtn = document.querySelector('#request');
+let inputValue = document.querySelector('#postID');
+let requestPost = document.querySelector('#request-post');
+let requestComment = document.querySelector('#request-comment');
+let url = "https://jsonplaceholder.typicode.com/posts";
+
+
 
 requestBtn.addEventListener('click', function() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => {
-        console.log(response);
+    let id = inputValue.value;
 
-        return response
+    console.log(id);
+    fetch(`${url}/${id}`)
+    .then(response => {
+        return response.json();
     })
     .then(data => {
-        data.json()
         console.log(data);
+        requestPost.innerHTML = JSON.stringify(data);
+        return getComments(id);
     })
 })
+
+function getComments(commentId) {
+    fetch(`${url}/${commentId}/comments`)
+    .then(response => {
+        return response.json();
+    })
+    .then(comments => {
+        requestComment.innerHTML = JSON.stringify(comments)
+        console.log(comments);
+    })
+}
